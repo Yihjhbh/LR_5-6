49205e595ce99873a86ad0c46334349b5a04f796
etoTimoxa <39565490+Yihjhbh@users.noreply.github.com>
2023-10-20 08:47:07 +0300

    Add files via upload

---
+++ 8.pas
@@ -0,0 +1,16 @@
+﻿program zad10;
+var a: array [1..20] of integer;
+ i:integer;
+ begin
+  Writeln('Исходный массив: ');
+  for i:=1 to 20 do
+   begin 
+a[i]:= -10 + random(21);
+   write(a[i]:4);
+   end;
+  Writeln;
+  Writeln('Массив без отрицательных элементов: ');
+  For i:=1 to 20 do
+   if a[i]>=0 then
+   Write(a[i]:3);
+ End.
